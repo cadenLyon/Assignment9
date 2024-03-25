87d17a15845e080b37937c7538c0a0e31158f028
Caden Lyon <lyon_caden@yahoo.com>
2024-03-24 18:31:10 -0600

    added drop downs

--- Problem3/src/main/java/com/example/problem3/HelloApplication.java
+++ Problem3/src/main/java/com/example/problem3/HelloApplication.java
@@ -1,8 +1,17 @@
 package com.example.problem3;
+/*
+Caden Lyon
+Computer Science II
+3/24/24
+ */
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +19,22 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Group group = new Group();
+        Scene scene = new Scene(group, 600, 400, Color.LIGHTSKYBLUE);
+        stage.setTitle("Problem 3");
+        String[] yearsList = {"2001", "2002", "2003", "2004", "2005", "2006"," 2007", "2008", "2009",
+                "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018"," 2019", "2020", "2021"};
+        String[] maleOrFemList = {"Male", "Female"};
+
+        ComboBox yearsListDropDown = new ComboBox<>(FXCollections.observableArrayList(yearsList));
+        ComboBox maleOrFemDropDown = new ComboBox<>(FXCollections.observableArrayList(maleOrFemList));
+
+
+
+
+
+
+        group.getChildren().addAll(yearsListDropDown, maleOrFemDropDown);
         stage.setScene(scene);
         stage.show();
     }
